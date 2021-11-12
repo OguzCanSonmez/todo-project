0a6070743a616ca1b77cbd024da2384a6543dc7e
Oğuz Can Sönmez <oguzcan.sonmez@gmail.com>
2021-11-12 17:17:03 +0300

    Add new todo item function completed

--- src/App.js
+++ src/App.js
@@ -9,14 +9,27 @@ const INITIAL_STATE = [
 function App() {
   // States
   const [tasks, setTask] = useState(INITIAL_STATE);
-  
+  const [newTask, setNewTask] = useState(""); // State for saving temporary input
+
+  // Adding new todo item
+  const addNew = (text) => {
+    if (text !== "") {
+      setTask([...tasks, { id: Date.now(), content: text, isDone: false }]);
+      setNewTask("");
+    } else {
+      alert("Empty value cant be inserted");
+    }
+  };
+
   return (
     <div>
       <h1>TO DO PROJECT</h1>
       <input
         type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
       />
-      <button>Add</button>
+      <button onClick={() => addNew(newTask)}>Add</button>
       <div className="items">
         {tasks.map((item) => (
           <div key={item.id} className="item">
